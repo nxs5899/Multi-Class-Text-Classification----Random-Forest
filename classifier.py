d9a0c810a8c6b7e0e3cddbaec7fab1015c7040fc
Nasir Safdari <nxs5899@rit.edu>
2018-02-20 15:57:36 -0700

    comments

--- classifier.py
+++ classifier.py
@@ -15,36 +15,42 @@ from sklearn.ensemble import RandomForestClassifier
 import sqlite3
 import pickle
 
+#connecting tot he database to load the training set data. the below database contains the commit messages and their corresponding labels
 try:
     conn = sqlite3.connect("training_V2.db")
 except Error as e:
     print(e)
 
+#reading the data from the table that contains the training set	
 df = pd.read_sql_query('SELECT * FROM filtered', conn)
 df.drop(['id'], 1, inplace=True)
 
 # print(df.head())
-
+# this block preprocess the text, removes the stop words and transform the text into vector space using tfidf vectorizer
 stemmer = PorterStemmer()
 words = stopwords.words("english")
 vectorizer = TfidfVectorizer(min_df= 3, stop_words="english", sublinear_tf=True)
-# clf = LinearSVC(C=1.0, penalty='l1', max_iter=3000, dual=False)
+
 df['cleaned'] = df['text'].apply(lambda x: " ".join([stemmer.stem(i) for i in re.sub("[^a-zA-Z]", " ", x).split() if i not in words]).lower())
 
+# this block is to split the dataset into training and testing set 
 X = df['cleaned']
 Y = df['class']
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.25)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20)
+
 
 pipeline = Pipeline([('vect', vectorizer),
                      ('chi',  SelectKBest(chi2, k=1000)),
                      ('clf', RandomForestClassifier())])
 
-
+# fitting our model and save it in a pickle for later use
 model = pipeline.fit(X_train, y_train)
 with open('RandomForest.pickle', 'wb') as f:
     pickle.dump(model, f)
-# xtrain = vectorizer.transform(X_test)
+
 ytest = np.array(y_test)
+
+# confusion matrix and classification report(precision, recall, F1-score)
 
 print(confusion_matrix(ytest, model.predict(X_test)))
 print(classification_report(ytest, model.predict(X_test)))
@@ -55,6 +61,8 @@ clf = model.named_steps['clf']
 feature_names = vectorizer.get_feature_names()
 feature_names = [feature_names[i] for i in chi.get_support(indices=True)]
 feature_names = np.asarray(feature_names)
+
+# in this case, I have 5 different classes:
 
 target_names = ['1', '2', '3', '4', '5']
 print("top 10 keywords per class:")
