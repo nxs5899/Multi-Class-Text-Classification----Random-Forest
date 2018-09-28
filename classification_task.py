618160135cdac022a5502f236afeeb75cadf7e2a
nxs5899 <nxs5899@rit.edu>
2018-09-28 12:22:52 -0400

    Update classification_task.py

--- classification_task.py
+++ classification_task.py
@@ -2,9 +2,8 @@ import pandas as pd
 import pickle
 from sqlite3 import Error
 import sqlite3
-# nltk.download('wordnet')
 
-
+# this database contains the commit messages that must be classified using the model built in the past step.
 try:
     conn = sqlite3.connect("myWork.db")
 except Error as e:
@@ -21,10 +20,9 @@ clf = pickle.load(pickle_load)
 
 def classify():
     list = []
-    for bit in strings:
-        bit = str(bit)
-        texty = [bit]
-        pred = clf.predict(texty)
+    for message in strings:
+        message1 = [str(message)]
+        pred = clf.predict(message1)
         list.append(pred[0])
     return list
 
